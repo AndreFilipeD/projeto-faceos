93e1dd268cd1954ed73c26c0f273be47f07307f1
André Filipe <117784318+AndreFilipeD@users.noreply.github.com>
2023-09-20 11:30:20 -0300

    Finalizando algumas correções no media-query

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,28 @@
-var rotLogo = window.document.querySelector('#anilogo')
+var rotLogo = this.window.document.querySelector('#anilogo')
+var titleName = this.window.document.querySelector('#facename')
 
-function logoup(){
-    
+function touchup(){
     rotLogo.style = 'filter: invert()'
 }
 
-function logodown(){
+function touchdown(){
     rotLogo.style = 'filter: none'
 }
 
+function touchupb(){
+    titleName.style = 'filter: invert()'
+}
+
+function touchdownb(){
+    titleName.style = 'filter: none'
+}
+
+function gohome(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
 const loop = setInterval(()=>{
-    rotLogo.style.transform= 'rotate('+ (window.scrollY)/5 +'deg)'  
+    rotLogo.style.transform= 'rotate('+ (window.scrollY)/3 +'deg)'  
 })
 
